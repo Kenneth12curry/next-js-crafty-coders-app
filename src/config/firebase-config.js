b089cc2a549a4f42db0cbc8c68e60fae79fea32e
kenneth <kenneth.adjete@ism.edu.sn>
2024-03-16 02:15:01 +0000

    third commit

---
+++ src/config/firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBlQCee7ju_kz2rOL4Y6gVi0ZXBe4H2Z10",
+  authDomain: "crafty-coders-a31cf.firebaseapp.com",
+  projectId: "crafty-coders-a31cf",
+  storageBucket: "crafty-coders-a31cf.appspot.com",
+  messagingSenderId: "235235968558",
+  appId: "1:235235968558:web:bb964678673a542e6c01bf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
